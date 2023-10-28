a8da8e3c20f75da87ebf46ed8ddc9f60c6643ed4
Ashish20cs17 <aak479900@gmail.com>
2023-10-29 00:33:27 +0530

    project

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php include("header.php");?>
+
+<div class="container-fluid">
+	<div class="row" id="wrapper">
+	<?php require_once'menubar.php';?>
+		<div class="col-sm-12">
+		<div class="panel panel-default">
+		<div class="panel-body">
+		<center>
+		<h1 style="color:#0B3861; font-family:Impact;">Mahatma Jyotiba Phule Rohilkhand University (MJPRU),Bareilly</h1>
+		<h3 style="color:#0B615E; font-family:Elephant;">Department of Computer Science & Information Technology</h3>
+		<img class="img-circle" alt="Image Preview" src="pics/logo.jpeg" />
+		</center>
+		</div>
+	</div>
+		<div class="panel panel-default">
+				<div class="panel-body">
+				<div class="media">
+				<a href="#" class="pull-left"><img alt="Bootstrap Media Preview" src="pics/chairman.jpeg" class="media-object" /></a>
+				<div class="media-body">
+					<h4 class="media-heading" style="font-family:Bodoni MT Condensed; color:#0489B1;">
+						Message from Chairman
+					</h4> Department of Computer Science & Information Technology, Mahatma Jyotiba Phule Rohilkhand University (MJPRU),Bareilly, is one of the leading Graduate and Post-graduate department in INDIA.The curriculum offered by our department has been designed in accordance with different renowned universities around the world. Recently with financial aid from World Bank and UGC worth 33 Million BDT, our department has been renovated to meet the highest standard of education and consequently, we now offer a world class learning experience. All of our class rooms and laboratories are equipped with modern audio-visual teaching-learning facilities and necessary experimental kits for undergraduate and graduate students. At the same time, we have extended the facilities to enhance our research activities in different branches of computer science and its engineering applications. Moreover, we frequently arrange workshop, seminar, academic contest of students and extra-curriculum activities to explore the merits of our students in different dimension. At the same time, we get our students involved in organizing these events to improve their leadership quality. We want our students to be capable of not in one dimension, but capable of multi-dimensions when they graduate. As a result, now our department is the right place for the students with dreams of becoming outstanding IT professionals and researchers. We are looking forward to welcome brilliant students.
+				</div>
+				</div>
+		</div>
+	</div>
+	</div>
+</div>
+</div>
+
+			
